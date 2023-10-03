6bbf40c3343b89e9a281cc776e3bb0429384845a
belltree <c0a22087e5@edu.teu.ac.jp>
2023-10-03 14:25:55 +0900

    練習１

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,8 +1,9 @@
+import random
 import sys
 import pygame as pg
 
 
-WIDTH, HEIGHT = 800, 450
+WIDTH, HEIGHT = 1600, 900
 
 
 def main():
@@ -11,7 +12,20 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    clock = pg.time.Clock()
+
+    #ばくだん
+    enn = pg.Surface((20, 20))
+    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
+    enn.set_colorkey((0,0,0))#黒を透明に
+
+    enn_rct=enn.get_rect()
+
+    x=random.randint(0,WIDTH)
+    y=random.randint(0,HEIGHT)
+    enn_rct.center=(x,y)
+    
+
+    clock = pg.time.Clock()    
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -20,6 +34,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(enn,enn_rct)  #ばくだんの試し
         pg.display.update()
         tmr += 1
         clock.tick(10)
